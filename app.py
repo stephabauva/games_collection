64c15b9307eb5273c83bc347cc60f8a2658e457c
sbv <stephane.baudry.vanaa@gmail.com>
2023-06-26 12:54:58 +0200

    ajout du lien vers la db

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from models import db
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///games.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+db.init_app(app)
+
+import views
+
+if __name__ == '__main__':
+    app.run(debug=True)
